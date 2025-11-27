e9c9acb0883c3b025da68c93acf9ec2635bec779
maisonregisford <147837924+maisonregisford@users.noreply.github.com>
2025-11-27 23:51:04 +0000

    FINAL CODE WEB DONE

--- app/components/SubscribeButton.js
+++ app/components/SubscribeButton.js
@@ -2,13 +2,11 @@
 
 export default function SubscribeButton() {
   return (
-    <a
-      href="https://buy.stripe.com/14A6oIb8Z9C20LW63b3VC00"
-      target="_blank"
-      rel="noopener noreferrer"
+    <button
+      onClick={() => (window.location.href = "/login?mode=signup")}
       className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-6 rounded inline-block"
     >
       Subscribe Now
-    </a>
+    </button>
   )
 }
